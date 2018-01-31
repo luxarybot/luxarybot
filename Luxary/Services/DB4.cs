aeab5ef0a06fd22ae7f482db4b7f24e4924289a2
Luxarybot <saintmelontje@gmail.com>
2018-01-31 15:05:00 +0100

    kappa

--- Luxary/Services/DB4.cs
+++ Luxary/Services/DB4.cs
@@ -6,7 +6,8 @@ namespace Luxary.Services
     {
         public int Id { get; set; }
         public ulong DiscordID { get; set; }
-        public int Money { get; set; }
+        public double Money { get; set; }
+        public string Name { get; set; }
         public Int32 Counter { get; set; }
         public string DailyGot { get; set; }
     }
